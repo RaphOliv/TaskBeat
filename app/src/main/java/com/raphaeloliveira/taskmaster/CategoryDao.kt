ea0655ff10a2e68531c7e44ca080c4adec5ecb44
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-05-23 23:52:31 +0100

    init commit

---
+++ app/src/main/java/com/raphaeloliveira/taskmaster/CategoryDao.kt
@@ -0,0 +1,26 @@
+package com.raphaeloliveira.taskmaster
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface CategoryDao {
+
+    @Query("SELECT * FROM categoryentity")
+    fun getAll(): List<CategoryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(categoryEntity: List<CategoryEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(categoryEntity: CategoryEntity)
+
+    @Delete
+    fun delete(categoryEntity: CategoryEntity)
+
+}
+
