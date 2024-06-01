3e423cce2044153d088a8ccfbe7987313f1d58a8
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-02 00:46:40 +0100

    init commit

--- app/src/main/java/com/raphaeloliveira/taskmaster/TaskMasterDataBase.kt
+++ app/src/main/java/com/raphaeloliveira/taskmaster/TaskMasterDataBase.kt
@@ -1,10 +1,12 @@
 package com.raphaeloliveira.taskmaster
 
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [CategoryEntity::class,TaskEntity::class], version = 5)
 abstract class TaskMasterDataBase : RoomDatabase() {
     abstract fun getTaskDao(): TaskDao
     abstract fun getCategoryDao(): CategoryDao
+
 }
