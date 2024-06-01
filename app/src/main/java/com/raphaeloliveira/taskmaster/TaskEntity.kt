699a896bc351ae9cfb8d89023913e3c661efad3c
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-02 00:44:59 +0100

    init commit

---
+++ app/src/main/java/com/raphaeloliveira/taskmaster/TaskEntity.kt
@@ -0,0 +1,21 @@
+package com.raphaeloliveira.taskmaster
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = CategoryEntity::class,
+            parentColumns = ["key"],
+            childColumns = ["category"]
+        )
+    ]
+)
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val category: String,
+    val name: String
+)
