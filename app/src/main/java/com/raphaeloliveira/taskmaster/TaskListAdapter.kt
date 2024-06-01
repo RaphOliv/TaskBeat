7712cde6a109207ff796ba5f7bb7b2d14b66fd7e
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-02 00:45:55 +0100

    init commit

---
+++ app/src/main/java/com/raphaeloliveira/taskmaster/TaskListAdapter.kt
@@ -0,0 +1,55 @@
+package com.raphaeloliveira.taskmaster
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskListAdapter :
+    ListAdapter<TaskUiData, TaskListAdapter.TaskViewHolder>(diffCallback()) {
+
+            private lateinit var callBack: (TaskUiData) -> Unit
+            fun setOnClickListener(onClick: (TaskUiData) -> Unit) {
+                callBack = onClick
+            }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+            return TaskViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+            val category = getItem(position)
+            holder.bind(category, callBack)
+        }
+
+        class TaskViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+            private val tvCategory = view.findViewById<TextView>(R.id.tv_category_name)
+            private val tvName = view.findViewById<TextView>(R.id.tv_task_name)
+            fun bind(task: TaskUiData, callBack: (TaskUiData) -> Unit) {
+                tvName.text = task.name
+                tvCategory.text = task.category
+
+                view.setOnClickListener {
+                    callBack.invoke(task)
+                }
+            }
+        }
+
+        class diffCallback  : DiffUtil.ItemCallback<TaskUiData>() {
+            override fun areItemsTheSame(oldItem: TaskUiData, newItem: TaskUiData): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: TaskUiData, newItem: TaskUiData): Boolean {
+                return oldItem.name == newItem.name
+                    && oldItem.category == newItem.category
+            }
+        }
+    }
+
+
+
