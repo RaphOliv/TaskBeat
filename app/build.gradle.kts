41c4c81456281ecb8f9fe6dd5fe291f81eba2bbf
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-06-20 22:08:05 +0100

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
-    /*viewBinding {
-        enabled = true
-    }*/
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -51,7 +50,17 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.core:core-splashscreen:1.0.1")
-    implementation ("com.airbnb.android:lottie:6.1.0")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    kapt ("androidx.lifecycle:lifecycle-compiler:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-service:2.8.2")
+    implementation ("androidx.lifecycle:lifecycle-process:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-reactivestreams-ktx:2.8.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.2")
 
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
@@ -59,10 +68,10 @@ dependencies {
     implementation("androidx.room:room-ktx:$room_version")
     implementation("androidx.room:room-guava:$room_version")
 
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("androidx.room:room-testing:$room_version")
-
-
+    
 }
